c13c83ad2ef7c308ed4ed3558fff4d57365bd896
vanshita <pamnanivanshita@gmail.com>
2026-01-29 02:57:40 +0530

    Fix delete project route

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Project = require("../models/Project");
 
+router.delete("/test", (req, res) => {
+  res.json({ message: "DELETE route works" });
+});
+
 router.post("/", async (req, res) => {
   try {
     const { img, title, role, info, link, stage, progress } = req.body;
@@ -29,11 +33,30 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
+  console.log("Received DELETE request for id:", req.params.id);
   try {
     const projects = await Project.find();
     res.status(200).json(projects);
   } catch (err) {
     res.status(500).json({ message: "ERROR IN FETCHING PROJECTS", err });
+  }
+});
+
+// router.delete("/:id", async (req, res) => {
+//   console.log("🔥 DELETE ROUTE HIT");
+//   console.log("ID:", req.params.id);
+//   return res.status(200).json({ message: "DELETE route works" });
+// });
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedProject = await Project.findByIdAndDelete(id);
+    if (!deletedProject) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    res.status(200).json({ message: "Project deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error in deleting project", error });
   }
 });
 
